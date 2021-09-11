a98b92bb7520937a02ebd567a25c41f708a4b71c
Hossein Moradi <ho3inmoradi1998@gmail.com>
2021-09-11 12:36:32 -0700

    define relationship

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $fillable = ['title','slug','body','category_id','user_id'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
     use HasFactory;
 }
