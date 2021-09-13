d83ddc8f99cf42c1c94bc5f191145fee1ad70dc7
Hossein Moradi <ho3inmoradi1998@gmail.com>
2021-09-13 13:08:50 -0700

    like and dislike system for reply question

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/question',\App\Http\Controllers\QuestionController::class);
 Route::apiResource('/category',\App\Http\Controllers\CategoryController::class);
 Route::apiResource('/question/{question}/reply',\App\Http\Controllers\ReplyController::class);
+
+Route::post('/like/{reply}',[\App\Http\Controllers\LikeController::class, 'likeIt']);
+Route::delete('/like/{reply}',[\App\Http\Controllers\LikeController::class, 'unlikeIt']);
