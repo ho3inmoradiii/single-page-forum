cca510e76084082fbdfa8baa38381a8f2f507bf3
ho3inmoradiii <ho3inmoradi1998@gmail.com>
2021-09-28 23:02:45 -0700

    realtime like and dislike system

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\LikeEvent;
 use App\Models\Like;
 use App\Http\Controllers\Controller;
 use App\Models\Reply;
@@ -16,10 +17,12 @@ class LikeController extends Controller
     }
     public function likeIt(Reply $reply){
         $reply->like()->create(['user_id' => Auth::id()]);
+        broadcast(new LikeEvent($reply->id,1))->toOthers();
     }
 
     public function unlikeIt(Reply $reply)
     {
         $reply->like()->where('user_id',Auth::id())->first()->delete();
+        broadcast(new LikeEvent($reply->id,0))->toOthers();
     }
 }
